500b2e1d26ee596bb31de55d3272d2d7cd580fe9
Alistair McCutcheon <chessnutter@gmail.com>
2022-07-10 15:17:43 +1000

    Split into train and test sets

--- src/dataset.py
+++ src/dataset.py
@@ -1,13 +1,34 @@
 from torch.utils.data import Dataset
 from PIL import Image
 import numpy as np
+import os
 
 
 class FaceDataset(Dataset):
-    def __init__(self, img_paths_file_path, transform=None):
-        with open(img_paths_file_path) as file:
-            img_paths = file.readlines()
-        self.img_paths = [img_path.strip() for img_path in img_paths]
+    @classmethod
+    def read_img_paths(cls, root):
+        img_paths = [os.path.join(root, img_name) for img_name in os.listdir(root)]
+        return np.array(img_paths)
+
+    @classmethod
+    def partition_img_paths(cls, root, partition_proportions=(1), shuffle=True):
+        img_paths = FaceDataset.read_img_paths(root)
+        np.random.shuffle(img_paths)
+        partition_counts = (np.array(partition_proportions) * len(img_paths)).astype(
+            np.uint32
+        )
+
+        out = []
+        partition_start_index = 0
+        for partition_count in partition_counts:
+            partition_end_index = partition_start_index + partition_count
+            out.append(img_paths[partition_start_index:partition_end_index])
+            partition_start_index += partition_count
+
+        return out
+
+    def __init__(self, img_paths, transform=None):
+        self.img_paths = img_paths
         self.transform = transform
 
     def __getitem__(self, index):
--- src/train.py
+++ src/train.py
@@ -1,7 +1,31 @@
+import tensorboard
+import torchvision
 from dataset import FaceDataset
-from torchvision.transforms import Compose, ToTensor
+from torchvision import transforms
+from torch.utils.tensorboard import SummaryWriter
+import torch
+from torch.utils.data import DataLoader
 
-transforms = Compose([ToTensor()])
-dataset = FaceDataset("data/img_paths.txt", transforms)
-print(type(dataset[10]))
-print(dataset[10].shape)
+transforms = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Resize((128, 128)),
+        transforms.CenterCrop((128, 128)),
+    ]
+)
+partitioned_img_paths = FaceDataset.partition_img_paths(
+    "data/img_align_celeba/", (0.8, 0.2)
+)
+train_dataset = FaceDataset(partitioned_img_paths[0], transforms)
+test_dataset = FaceDataset(partitioned_img_paths[1], transforms)
+
+batch_size = 32
+train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size, shuffle=True)
+
+writer = SummaryWriter()
+train_batch_grid = torchvision.utils.make_grid(next(iter(train_dataloader)))
+test_batch_grid = torchvision.utils.make_grid(next(iter(test_dataloader)))
+writer.add_image("sample_batch/train", train_batch_grid, 0)
+writer.add_image("sample_batch/test", test_batch_grid, 0)
+writer.close()
