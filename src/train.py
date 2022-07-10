3f12d43177075875d7965be37dc374610f1678dd
Alistair McCutcheon <chessnutter@gmail.com>
2022-07-10 15:26:59 +1000

    Added seperate transforms for train and test set

--- src/train.py
+++ src/train.py
@@ -6,18 +6,22 @@ from torch.utils.tensorboard import SummaryWriter
 import torch
 from torch.utils.data import DataLoader
 
-transforms = transforms.Compose(
+required_transforms = transforms.Compose(
     [
         transforms.ToTensor(),
         transforms.Resize((128, 128)),
         transforms.CenterCrop((128, 128)),
     ]
 )
+train_transforms = transforms.Compose(
+    [*required_transforms.transforms, transforms.GaussianBlur(15)]
+)
+
 partitioned_img_paths = FaceDataset.partition_img_paths(
     "data/img_align_celeba/", (0.8, 0.2)
 )
-train_dataset = FaceDataset(partitioned_img_paths[0], transforms)
-test_dataset = FaceDataset(partitioned_img_paths[1], transforms)
+train_dataset = FaceDataset(partitioned_img_paths[0], train_transforms)
+test_dataset = FaceDataset(partitioned_img_paths[1], required_transforms)
 
 batch_size = 32
 train_dataloader = DataLoader(train_dataset, batch_size, shuffle=True)
