d4d4f0a7534436f2e6111939f2005868f4c9a206
Alistair McCutcheon <chessnutter@gmail.com>
2022-07-10 13:03:21 +1000

    Added transforms

--- src/dataset.py
+++ src/dataset.py
@@ -4,14 +4,18 @@ import numpy as np
 
 
 class FaceDataset(Dataset):
-    def __init__(self, img_paths_file_path):
+    def __init__(self, img_paths_file_path, transform=None):
         with open(img_paths_file_path) as file:
             img_paths = file.readlines()
         self.img_paths = [img_path.strip() for img_path in img_paths]
+        self.transform = transform
 
     def __getitem__(self, index):
         img = Image.open(self.img_paths[index])
-        return np.array(img)
+        img = np.array(img)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img
 
     def __len__(self):
         return len(self.img_paths)
--- src/train.py
+++ src/train.py
@@ -1,5 +1,7 @@
 from dataset import FaceDataset
+from torchvision.transforms import Compose, ToTensor
 
-dataset = FaceDataset("data/img_paths.txt")
+transforms = Compose([ToTensor()])
+dataset = FaceDataset("data/img_paths.txt", transforms)
 print(type(dataset[10]))
 print(dataset[10].shape)
