35b427c93db3e57c923025b99ab578d78503020e
Alistair McCutcheon <chessnutter@gmail.com>
2022-07-11 15:33:15 +1000

    Added optional transform.Normalize to imagenet values

--- src/train.py
+++ src/train.py
@@ -13,6 +13,7 @@ required_transforms = transforms.Compose(
         transforms.ToTensor(),
         transforms.Resize((img_size, img_size)),
         transforms.CenterCrop((img_size, img_size)),
+        # transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
     ]
 )
 
