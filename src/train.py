c4113ae34e5085c65b454a2f7238f191349dc52b
Alistair McCutcheon <chessnutter@gmail.com>
2022-07-11 17:02:41 +1000

    Initialised weights and bias of generator and discriminator

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+from operator import mod
 import torchvision
 from dataset import FaceDataset
 from torchvision import transforms
@@ -6,6 +7,18 @@ import torch
 from torch.utils.data import DataLoader
 from discriminator import Discriminator
 from generator import Generator
+from torch import nn
+
+
+def parameters_init(module):
+    if isinstance(module, nn.ConvTranspose2d):
+        nn.init.normal_(module.weight, 0.0, 0.02)
+    elif isinstance(module, nn.Conv2d):
+        nn.init.normal_(module.weight, 0.0, 0.02)
+    elif isinstance(module, nn.BatchNorm2d):
+        nn.init.normal_(module.weight, 1, 0.02)
+        nn.init.constant_(module.bias, 0)
+
 
 img_size = 64
 required_transforms = transforms.Compose(
@@ -36,6 +49,11 @@ writer.add_image("sample_batch/test", test_batch_grid, 0)
 
 noise_size = 100
 generator_model = Generator(noise_size=noise_size, number_generator_features=64)
+generator_model.apply(parameters_init)
+
+discriminator_model = Discriminator(number_discriminator_features=64)
+discriminator_model.apply(parameters_init)
+
 batch_size = 32
 noise = torch.randn(batch_size, noise_size, 1, 1)
 generated_imgs = generator_model(noise)
@@ -43,7 +61,6 @@ generated_imgs = generator_model(noise)
 generated_imgs_grid = torchvision.utils.make_grid(generated_imgs)
 writer.add_image("generated_images", generated_imgs_grid, 0)
 
-discriminator_model = Discriminator(number_discriminator_features=64)
 chances = discriminator_model(generated_imgs)
 chances = chances.reshape(-1)
 print(chances)
